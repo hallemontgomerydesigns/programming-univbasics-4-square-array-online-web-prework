842bccee7d2831787898ddd1bfffb33b579e5fa6
Halle Montgomery <h.rikkele.montgomery@gmail.com>
2019-09-04 20:31:45 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,9 @@
 def square_array(array)
-   counter = 0 
-   while counter < array.length do
-      (array[counter])**2
-      counter += 1
-   end
+  squared = []
+  count = 0
+  while count < array.size do
+    squared << (array[count] ** 2)
+    count = count + 1
+  end
+  squared
 end
