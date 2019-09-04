b0406b9539026228a43b1d4469ae7dc287c66117
Halle Montgomery <h.rikkele.montgomery@gmail.com>
2019-09-04 20:10:26 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   array = [1,2,3]
    counter = 0 
    while counter < array.length do
-     puts array[counter]
+     puts array[counter].sqrt
       counter += 1
    end
 end
